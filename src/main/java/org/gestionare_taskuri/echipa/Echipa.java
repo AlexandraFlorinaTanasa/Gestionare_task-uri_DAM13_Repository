7164c448127c0973126d5694eb8528d612993667
Alexandra Florina Tanasă <160071375+AlexandraFlorinaTanasa@users.noreply.github.com>
2024-12-11 08:30:46 +0200

    Add files via upload

---
+++ src/main/java/org/gestionare_taskuri/echipa/Echipa.java
@@ -0,0 +1,51 @@
+package org.gestionare_taskuri.echipa;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Getter @Setter
+public class Echipa {
+
+
+@Id @GeneratedValue @NonNull
+    private Integer idEchipa;
+  @NonNull private Specializare specializare;
+    @NonNull  private String abilitati;
+ @OneToMany
+    private List<Angajat> angajati = new ArrayList<Angajat>();
+
+@OneToOne
+     @NonNull private TeamLeader teamLeader;
+
+    // properties from bean accessors
+    public Echipa(Integer idEchipa, Specializare specializare, String competente) {
+        super();
+        this.idEchipa = idEchipa;
+        this.specializare = specializare;
+        this.abilitati = competente;
+    }
+    //
+    public Echipa(Integer idEchipa, Specializare specializare) {
+        super();
+        this.idEchipa = idEchipa;
+        this.specializare = specializare;
+    }
+    // polimorfism parametrizare
+    public void addAngajat(Angajat angajat){
+        this.angajati.add(angajat);
+    }
+
+    public enum Specializare {
+        BACKEND, FRONTEND, DATABASE;
+    }
+}
+
+
