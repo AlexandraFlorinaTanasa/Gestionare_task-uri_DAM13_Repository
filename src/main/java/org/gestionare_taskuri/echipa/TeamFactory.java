1b8d04786874984119d24f181bb683f427adb336
Alexandra Florina Tanasă <160071375+AlexandraFlorinaTanasa@users.noreply.github.com>
2025-01-15 15:15:05 +0200

    Delete src/main/java directory

--- src/main/java/org/gestionare_taskuri/echipa/TeamFactory.java
+++
@@ -1,9 +0,0 @@
-package org.gestionare_taskuri.echipa;
-
-
-public class TeamFactory {
-    public Echipa buildTeam(Integer idEchipa) {
-        Echipa echipa = new Echipa(idEchipa, Echipa.Specializare.BACKEND);
-        return echipa;
-    }
-}
